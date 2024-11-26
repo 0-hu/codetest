836f6854565213e4e2c9ef75ab5309c42a55ce65
Young-Hu Kwon <yh.kwon@coweaver.co.kr>
2024-11-26 09:48:27 +0900

    add 2~4

---
+++ bj_1001.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+
+    int num1, num2;
+
+    std::cin >> num1 >> num2;
+
+    int result = num1 - num2;
+
+    std::cout << result << '\n';
+
+    return 0;
+
+}
---
+++ bj_10998.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    int n1, n2;
+
+    std::cin >> n1 >> n2;
+
+    int result = n1 * n2;
+    
+    std::cout << result << '\n';
+
+    return 0;
+
+}
